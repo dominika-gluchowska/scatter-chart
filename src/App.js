ad9d070f89d634d86b0c808123702feb28786952
Dominika Gluchowska <gluchowska.dominika@gmail.com>
2019-04-17 21:27:27 +0200

    chart extracted to separate component

--- src/App.js
+++ src/App.js
@@ -1,112 +1,42 @@
 import React, { Component } from 'react';
-import {scaleLinear} from 'd3-scale';
-import {axisLeft, axisBottom} from 'd3-axis';
-import {select} from 'd3-selection';
-import {line as lineD3} from 'd3-shape';
-import {extent} from 'd3-array';
+import create_data from './createData'
+import ScatterChart from './components/ScatterChart.jsx'
 
 import './App.css';
-import create_data from './createData'
 
+const dataMap =  {
+    dataSourceA: create_data(50),
+    dataSourceB: create_data(40),
+    dataSourceC: create_data(60)
+}
 class App extends Component {
 
-  componentDidMount(){
-    var margin = {
-          top: 20,
-          right: 20,
-          bottom: 30,
-          left: 40
-        },
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+    state = {
+        selectedDataSource: dataMap.dataSourceA,
+        selectedValue: 'dataSourceA'
+    }
 
-    var x = scaleLinear()
-        .range([0, width]);
-
-    var y = scaleLinear()
-        .range([height, 0]);
-
-    var xAxis = axisBottom()
-        .scale(x)
-
-    var yAxis = axisLeft()
-        .scale(y)
-
-    var svg = select("#chartHere").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var data = create_data(50);
-
-    data.forEach(function(d) {
-      d.x = +d.x;
-      d.y = +d.y;
-      d.yhat = +d.yhat;
-    });
-
-    var line = lineD3()
-        .x(function(d) {
-          return x(d.x);
+    changeDataSource = (event) => {
+        this.setState({
+            selectedDataSource: dataMap[event.target.value],
+            selectedValue: event.target.value
         })
-        .y(function(d) {
-          return y(d.yhat);
-        });
 
-    x.domain(extent(data, function(d) {
-      return d.x;
-    }));
-    y.domain(extent(data, function(d) {
-      return d.y;
-    }));
+        console.log(this.state.selectedDataSource[0])
+    }
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .append("text")
-        .attr("class", "label")
-        .attr("x", width)
-        .attr("y", -6)
-        .style("text-anchor", "end")
-        .text("X-Value");
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("class", "label")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Y-Value")
-
-    svg.selectAll(".dot")
-        .data(data)
-        .enter().append("circle")
-        .attr("class", "dot")
-        .attr("r", 3.5)
-        .attr("cx", function(d) {
-          return x(d.x);
-        })
-        .attr("cy", function(d) {
-          return y(d.y);
-        });
-
-    svg.append("path")
-        .datum(data)
-        .attr("class", "line")
-        .attr("d", line);
-  }
 
   render() {
-    return (
-      <div className="App" id="chartHere">
-
-      </div>
-    );
+      const {selectedValue, selectedDataSource} = this.state;
+    return [
+      <div className="App" id="chartHere"> </div>,
+        <ScatterChart data={selectedDataSource} className={selectedValue}/>,
+        <select onChange={this.changeDataSource} value={selectedValue}>
+          <option value="dataSourceA">Source 1</option>
+          <option value="dataSourceB">Source 2</option>
+          <option value="dataSourceC">Source 3</option>
+        </select>
+    ];
   }
 }
 
---
+++ src/components/ScatterChart.jsx
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import {scaleLinear} from 'd3-scale';
+import {axisLeft, axisBottom} from 'd3-axis';
+import {select} from 'd3-selection';
+import {line as lineD3} from 'd3-shape';
+import {extent} from 'd3-array';
+
+import './ScatterChart.css'
+
+var margin = {
+        top: 20,
+        right: 20,
+        bottom: 30,
+        left: 40
+    },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = scaleLinear()
+    .range([0, width]);
+
+var y = scaleLinear()
+    .range([height, 0]);
+
+var xAxis = axisBottom()
+    .scale(x)
+
+var yAxis = axisLeft()
+    .scale(y)
+
+class ScatterChart extends Component {
+
+    componentDidMount() {
+        const { data, className } = this.props;
+
+        var svg = select("#chartHere").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        data.forEach(function(d) {
+            d.x = +d.x;
+            d.y = +d.y;
+            d.yhat = +d.yhat;
+        });
+
+        var line = lineD3()
+            .x(function(d) {
+                return x(d.x);
+            })
+            .y(function(d) {
+                return y(d.yhat);
+            });
+
+        x.domain(extent(data, function(d) {
+            return d.x;
+        }));
+        y.domain(extent(data, function(d) {
+            return d.y;
+        }));
+
+        //create axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("x", width)
+            .attr("y", -6)
+            .style("text-anchor", "end")
+            .text("X-Value");
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Y-Value")
+
+
+        svg.selectAll(".dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("class", () => ("dot " + className))
+            .attr("r", 4.5)
+            .attr("cx", function(d) {
+                return x(d.x);
+            })
+            .attr("cy", function(d) {
+                return y(d.y);
+            });
+
+        /*svg.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);*/
+    }
+
+    componentDidUpdate(){
+
+                const {data, className} = this.props;
+
+                // Scale the range of the data again
+                x.domain(extent(data, (d) =>  d.x ));
+                y.domain(extent(data, (d) => d.y));
+
+                // Select the section we want to apply our changes to
+                var svg = select("#chartHere svg")
+
+                // Make the changes
+                svg.selectAll(".dot")
+                    .attr("class", () => ("dot " + className))
+                    .attr("cx", function(d) {
+                    return x(d.x);
+                    })
+                    .attr("cy", function(d) {
+                        return y(d.y);
+                    });
+                svg.select(".x.axis") // change the x axis
+                    .call(xAxis);
+                svg.select(".y.axis") // change the y axis
+                    .call(yAxis);
+
+
+
+    }
+
+    render() {
+        return (
+            <div className="chart" id="chartHere"></div>
+    );
+    }
+}
+
+export default ScatterChart;
